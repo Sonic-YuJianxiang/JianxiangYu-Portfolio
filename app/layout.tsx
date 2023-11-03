7a3332781a2241201b2f6d82802dba8b9a8afff3
Sonic-YuJianxiang <1347256906@qq.com>
2023-11-03 23:37:32 +0200

    finnish Header part

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,15 +12,16 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
         <div className='bg-[#5eead4] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full
         blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#86efac] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full
         blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Header />
         {children}
       </body>
     </html>
